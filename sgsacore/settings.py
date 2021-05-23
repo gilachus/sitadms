ea98b98b2205ad9bcd5f09d546fd0bf5efaba90f
gilachus <53192577+gilachus@users.noreply.github.com>
2021-05-23 17:38:28 -0500

    Update settings.py

--- sgsacore/settings.py
+++ sgsacore/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n0k75f+s(f%0f*j*+0bfnb5fv6rhk^8_emk%u1#_hc@v2ws2^-'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
